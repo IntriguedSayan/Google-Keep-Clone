daca468e94516e3d1430fc1b1d4e55b0278f8162
IntriguedSayan <sayan.mid590@gmail.com>
2024-07-20 15:39:45 +0530

    feat:Creating SliderDrawer on progress

--- project/src/components/SliderDrawer.tsx
+++ project/src/components/SliderDrawer.tsx
@@ -1,12 +1,52 @@
+// Sidebar.tsx
+import React, { useState } from 'react';
+import './SliderDrawer.css';
+import { FaHome, FaInfo, FaBell, FaTag, FaPen, FaDownload, FaTrash } from 'react-icons/fa';
+
 export const SliderDrawer: React.FC = () => {
-  return (
-  <>
-    <div>
-         Hello;   
-    </div>
-  </>
-  
-  
-  )
-  ;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className={`sidebar-container ${isOpen ? 'open' : ''}`}>
+            <button onClick={toggleSidebar} className="toggle-button">
+                ☰
+            </button>
+            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                <ul>
+                    <li>
+                        <FaHome />
+                        {isOpen && <span>Home</span>}
+                    </li>
+                    <li>
+                        <FaBell />
+                        {isOpen && <span>Notifications</span>}
+                    </li>
+                    <li>
+                        <FaTag />
+                        {isOpen && <span>Tags</span>}
+                    </li>
+                    <li>
+                        <FaInfo />
+                        {isOpen && <span>About</span>}
+                    </li>
+                    <li>
+                        <FaPen />
+                        {isOpen && <span>Edit</span>}
+                    </li>
+                    <li>
+                        <FaDownload />
+                        {isOpen && <span>Download</span>}
+                    </li>
+                    <li>
+                        <FaTrash />
+                        {isOpen && <span>Trash</span>}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
