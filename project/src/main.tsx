ec4e603ecf6d032e57a5a8111bad359c60396da1
IntriguedSayan <sayan.mid590@gmail.com>
2024-07-23 23:16:48 +0530

    feat:fixed deployment errors & removed context

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,18 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { SidebarContextProvider } from "./context/SidebarContext.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <BrowserRouter>
-      <SidebarContextProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </SidebarContextProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </>
 );
